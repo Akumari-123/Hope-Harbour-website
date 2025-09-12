b7e07018bed96532041b17a309340950cb9ce09c
Anjali Gupta <anjaligupta2072005@gmail.com>
2025-09-12 16:55:56 +0000

    ho gya ab kya kre ye btao

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -9,9 +10,10 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
+        protocol: 'httpshttps',
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
